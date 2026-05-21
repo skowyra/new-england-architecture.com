1d876aa72f590ccdf8020326d345bf4c42182e7d
James Skowyra <jamesskowyra@Jamess-Mac-mini.local>
2026-05-20 20:00:42 -0400

    simplified

--- components/calendar/config.js
+++
@@ -1,8 +0,0 @@
-// IMPORTANT: This API key should be restricted in Google Cloud Console
-// Restrict it to your domain only to prevent unauthorized use
-// https://console.cloud.google.com/apis/credentials
-
-const CALENDAR_CONFIG = {
-  apiKey: 'AIzaSyCMjkTrsr24HoS1e4pOim8FPcAPsbWvwCc', // TODO: Restrict this key in Google Console
-  calendarId: 'mackenziejamesstudio68@gmail.com'
-};
--- components/header/mj-header.js
+++ components/header/mj-header.js
@@ -22,7 +22,6 @@ class MJHeader extends HTMLElement {
           <nav>
             <ul>
               <li><a href="index.html">Home</a></li>
-              <li><a href="portfolio.html">Portfolio</a></li>
               <li><a href="about.html">About</a></li>
               <li><a href="contact.html">Contact</a></li>
             </ul>
